b68de6928d60e4a38f6cbee689ac6e29e2810c1d
Hisotau <suzuyac3w@gmail.com>
2024-04-17 19:52:22 +0300

    Изменена структура кода

--- src/main/java/org/hisotau/petsimulator/commands/summonPetCMD.java
+++ src/main/java/org/hisotau/petsimulator/commands/summonPetCMD.java
@@ -3,6 +3,7 @@ package org.hisotau.petsimulator.commands;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabExecutor;
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.hisotau.petsimulator.PetManager.PetRarity;
@@ -19,30 +20,38 @@ import java.util.List;
 public class summonPetCMD implements TabExecutor {
     @Override
     public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
-
-        if(!(commandSender instanceof Player)) {
+        if (!(commandSender instanceof Player)) {
             System.out.println("Команда вызвана не игроком");
             return false;
         }
 
         Player player = (Player) commandSender;
         PetManager pets = new PetManager();
-        var PetType = PetSimulator.getInstance().getConfig().getConfigurationSection("CUSTOM_PETS." +strings[0].toUpperCase()+ ".");
-        String name = PetType.getString("Name");
-        EntityType entity = EntityType.valueOf("Entity");
-        PetRarity rarity = PetRarity.valueOf("Rarity");
-        if (name != null) {
-            String coloredName = rarity + name;
-            pets.spawnPet(coloredName,entity,player.getLocation());
-            player.sendMessage("§9§lPetSimulator // §RВы успешно призвали: " + coloredName);
-        }
-        else {
-            System.out.println("Такого типа питомца не существует");
-            return false;
+        var PetType = PetSimulator.getInstance().getConfig().getConfigurationSection("CUSTOM_PETS." + strings[0].toUpperCase());
+
+        if (PetType != null) {
+            handleValidPetType(PetType, player, pets);
+        } else {
+            handleInvalidPetType(player);
         }
 
         return true;
     }
+
+    private void handleValidPetType(ConfigurationSection petType, Player player, PetManager pets) {
+        String name = petType.getString("Name");
+        EntityType entity = EntityType.valueOf(petType.getString("Entity"));
+        PetRarity rarity = PetRarity.valueOf(petType.getString("Rarity"));
+
+        String coloredName = rarity.getColor() + name;
+        pets.spawnPet(coloredName, entity, player.getLocation());
+        player.sendMessage("§9§lPetSimulator // §RВы успешно призвали: " + coloredName);
+    }
+
+    private void handleInvalidPetType(Player player) {
+        player.sendMessage("§9§lPetSimulator // §cТакого питомца не существует");
+    }
+
 
     @Override
     public @Nullable List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
